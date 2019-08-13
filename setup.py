9d61de59b11638a53345018c1d5528daffd83630
antonio <a.caristia@gmail.com>
2019-08-13 14:44:16 +0200

    Add correct url in setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup
 setup(
     name = "3183",
     version = "0.0.1",
-    author = "Antonio Caristia",
+    author = "Antonio Caristia (kinderp)",
     author_email = "a.caristia@gmail.com",
     description = ("mvc2pdf in a django style"),
     license = "GPL",
     keywords = "",
+    url = "https://github.com/kinderp/3183",
     packages=['3183', '3183.examples', '3183.fields', '3183.models', '3183.styles', '3183.utils', '3183.views'],
     install_requires=[
           'Pillow','reportlab',
