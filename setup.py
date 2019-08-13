d6e3d68ed31f1993d1abf22e9c7bd2b97e3fb2a5
antonio <a.caristia@gmail.com>
2019-08-13 15:17:03 +0200

    ...

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 # packages=['3183', '3183.examples', '3183.fields', '3183.models', '3183.styles', '3183.utils', '3183.views'],
 
 setup(
-    name = "_3183_",
+    name = "three18three",
     version = "0.0.1",
     author = "Antonio Caristia (kinderp)",
     author_email = "a.caristia@gmail.com",
