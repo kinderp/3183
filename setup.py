d7f2d29ee0c88e5151d3b3581ea708cb02466224
antonio <a.caristia@gmail.com>
2019-08-13 14:39:04 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name = "3183",
+    version = "0.0.1",
+    author = "Antonio Caristia",
+    author_email = "a.caristia@gmail.com",
+    description = ("mvc2pdf in a django style"),
+    license = "GPL",
+    keywords = "",
+    url = "http://packages.python.org/justpith",
+    packages=['3183', '3183.examples', '3183.fields', '3183.models', '3183.styles', '3183.utils', '3183.views'],
+    install_requires=[
+          'Pillow','reportlab',
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
+
