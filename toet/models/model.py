2f5695706402a75abd3f93cf5627909d6675b037
antonio <a.caristia@gmail.com>
2019-08-23 11:43:35 +0200

    Add _() translation function in base class Model
    _() function is responsible of translations.
    Every __t class atributes will be translated with _() call.
    
    _() uses 4 env vars:
    
     - TOET_TRANSLATIONS_DIR - files vocabularies' path
     - TOET_SRC_LANG - source translation lang
     - TOET_DEST_LANG - destination translation lang
     - TOET_LOAD_VOCABULARIES - load translations from disk or web

--- toet/models/model.py
+++ toet/models/model.py
@@ -1,10 +1,15 @@
 from abc import ABC
 
+import os
+
+from toet.utils import Translator
+
 class Model(ABC):
     """
-    Defined here interface for model types 
+    Defined here interface for model types
     """
-    def __init__(self, **kwargs):
+
+    def __init__(self, __t=None, **kwargs):
         """
         Init generic model
 
@@ -12,4 +17,23 @@ class Model(ABC):
             kwargs: it's a dict that will contain data model from controller obj
         """
         self.__dict__.update(kwargs)
+
+    def _(self, child, __t):
+        if __t is not None:
+            CLIENT_TRANSLATIONS_DIR = os.getenv('TOET_TRANSLATIONS_DIR')
+            SRC_LANG = os.getenv('TOET_SRC_LANG', 'en')
+            DEST_LANG = os.getenv('TOET_DEST_LANG', 'it')
+            LOAD_FROM_DISK = bool(int(os.getenv('TOET_LOAD_VOCABULARIES')))
+            if LOAD_FROM_DISK:
+                translated__t = Translator(src=SRC_LANG, dest=DEST_LANG,
+                                           disk=True, write=False,
+                                           translation_dir=CLIENT_TRANSLATIONS_DIR).translate(child)
+                #_translate_from_disk(child)
+            else:
+                translated__t = Translator(src=SRC_LANG, dest=DEST_LANG,
+                                           disk=False,
+                                           write=False).translate(__t)
+                #bulk_translate_from_web(__t)
+            self.__dict__.update(translated__t)
+            self.__dict__.update({'_t': translated__t})
 
