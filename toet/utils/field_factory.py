fb949dbd366129aec87be7b8c1c996427480d4b4
antonio <a.caristia@gmail.com>
2019-10-11 10:50:53 +0200

    Fix in utils/FieldFactory.py

--- toet/utils/field_factory.py
+++ toet/utils/field_factory.py
@@ -5,18 +5,33 @@ class FieldFactory():
     @staticmethod
     def create(class_name, data):
 
-        if isinstance(data, dict):
-            all_type = {
-                "TextField": TextField(**data),
-                "BulletTextField": BulletTextField(**data),
-                "ImageField": ImageField(**data) 
-            }
-        else:
-            all_type = {
-                "TextField": TextField(*data),
-                "BulletTextField": BulletTextField(*data),
-                "ImageField": ImageField(*data) 
-            }
+        #if isinstance(data, dict):
+        #    all_type = {
+        #        "TextField": TextField(**data),
+        #        "BulletTextField": BulletTextField(**data),
+        #        "ImageField": ImageField(**data) 
+        #    }
+        #else:
+        #    all_type = {
+        #        "TextField": TextField(*data),
+        #        "BulletTextField": BulletTextField(*data),
+        #        "ImageField": ImageField(*data) 
+        #    }
 
-        return all_type[class_name]
+        if isinstance(data, dict):
+                if class_name == 'TextField':
+                        return TextField(**data)
+                elif class_name == 'BulletTextField':
+                        return BulletTextField(**data)
+                elif class_name == 'ImageField':
+                        return ImageField(**data)
+        else:
+                if class_name == 'TextField':
+                        return TextField(*data)
+                elif class_name == 'BulletTextField':
+                        return BulletTextField(*data)
+                elif class_name == 'ImageField':
+                        return ImageField(*data)
+
+        #return all_type[class_name]
 
