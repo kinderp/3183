cad12432d74230fac196a722787490a9c59cbaee
antonio <a.caristia@gmail.com>
2019-10-31 17:27:40 +0100

    Workaround to solve blurred chars issue in Image

--- toet/fields/__init__.py
+++ toet/fields/__init__.py
@@ -2,3 +2,4 @@ from .field import Field
 from .text_field import TextField
 from .bullettext_field import BulletTextField
 from .image_field import ImageField
+from .image_field import LowImageField
--- toet/fields/image_field.py
+++ toet/fields/image_field.py
@@ -1,6 +1,24 @@
 from reportlab.platypus import Image
+from reportlab.platypus import Flowable
 
 from .field import Field
+
+class MyFlowable(Flowable):
+    """
+     A Custom Flowable
+     """
+    def __init__(self, img,width, height=0):
+        Flowable.__init__(self)
+        self.width = width
+        self.height = height
+        self.img = img
+
+    #def wrap(self, *args):
+    #    return (self.width, self.height)
+
+    def draw(self):
+        self.canv.drawImage(self.img, 0, 0, height=self.height, width=self.width)
+
 
 class ImageField(Field):
     """
@@ -13,4 +31,18 @@ class ImageField(Field):
 
     def render(self):
         return Image(self.path, width=self.width, height=self.height)
+
+
+class LowImageField(Field):
+    """
+    Reportlab Image Wrapper
+    """
+
+    def __init__(self, path='', width=None, height=None):
+        self.path = path
+        self.width = width
+        self.height = height
+
+    def render(self):
+        return MyFlowable(self.path, width=self.width, height=self.height)
 
